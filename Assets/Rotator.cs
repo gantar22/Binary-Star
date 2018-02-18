2f6980471966c491c840a8cf50e67b6d7490bd0b
Nathan Carter Williams <nathancarterwilliams@gmail.com>
2018-02-18 13:19:18 -0500

    added gitignore

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -17,7 +17,9 @@ public class Rotator : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		joy = new Vector2(XCI.GetAxisRaw(XboxAxis.LeftStickX,ctlr),XCI.GetAxisRaw(XboxAxis.LeftStickY,ctlr));
+		keyboard();
+
+		joy += new Vector2(XCI.GetAxisRaw(XboxAxis.LeftStickX,ctlr),XCI.GetAxisRaw(XboxAxis.LeftStickY,ctlr));
 		if (joy.x == 0 && joy.y == 0) return;
 		if(joy.y == 0) targetTheta = 180 * joy.x * -1;
 		targetTheta = joy.x == 0 ? 90 * joy.y : (360 / (2 * Mathf.PI)) * Mathf.Atan(joy.y / joy.x);
@@ -42,5 +44,16 @@ public class Rotator : MonoBehaviour {
 
 	float mod(float m,float n){
 		return m % n < 0 ? m % n + n : m % n;
+	}
+
+
+	void keyboard(){
+		if(ctlr == XboxController.First){
+			joy.x = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0);
+			joy.y = (Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0);
+		} else {
+			joy.x = (Input.GetKey(KeyCode.RightArrow) ? 1 : 0) + (Input.GetKey(KeyCode.LeftArrow) ? -1 : 0);
+			joy.y = (Input.GetKey(KeyCode.UpArrow) ? 1 : 0)    + (Input.GetKey(KeyCode.DownArrow) ? -1 : 0);
+		}
 	}
 }
