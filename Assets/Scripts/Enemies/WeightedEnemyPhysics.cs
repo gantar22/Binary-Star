01b08f97b7eff9bb748d4eebdaa0005bc96829a6
Dmcdominic <dmcdominic@gmail.com>
2018-03-01 18:01:10 -0500

    Enemies Rotate Towards the Direction of their Acceleration

--- Assets/Scripts/Enemies/WeightedEnemyPhysics.cs
+++ Assets/Scripts/Enemies/WeightedEnemyPhysics.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class WeightedEnemyPhysics : MonoBehaviour {
 
-	// Settings/properties, set by enemy controller scripts
+	// Settings/properties
 	//[SerializeField]
-	private float drag = 0.4f;
+	//private float drag = 0.4f;
+	private float turnRate = 15f;
+	private float angleLeeway = 3f;
 
 	// Other variables
 	[HideInInspector]
@@ -24,14 +26,42 @@ public class WeightedEnemyPhysics : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D> ();
 	}
 	
-	// Update is called once per frame
+	// Called once per frame
 	void Update () {
-		Vector2 pos = new Vector2 (transform.position.x, transform.position.y);
+		Vector2 pos = transform.position;
 
-		/* if (acceleration.magnitude == 0f) {
-			acceleration = velocity * drag;
-		} */
 
+		// Rotate towards direction of acceleration
+		float currentAngle = transform.eulerAngles.z;
+		if (currentAngle > 180f) {
+			currentAngle -= 360f;
+		} else if (currentAngle < -180f) {
+			currentAngle += 360f;
+		}
+
+		float targetAngle = Mathf.Atan2 (acceleration.y, acceleration.x) * Mathf.Rad2Deg;
+		if (targetAngle > 180f) {
+			targetAngle -= 360f;
+		} else if (targetAngle < -180f) {
+			targetAngle += 360f;
+		}
+
+		float diff = targetAngle - currentAngle;
+		if (diff > 180f) {
+			diff = diff - 360f;
+		} else if (diff < -180f) {
+			diff = diff + 360f;
+		}
+
+		if (Mathf.Abs(diff) > Mathf.Abs(angleLeeway)) {
+			//diff = diff / Mathf.Abs (diff);
+			float deltaTheta = Mathf.Pow(Mathf.Abs(diff), 0.6f) * Mathf.Sign(diff) * turnRate * Time.deltaTime;
+			//transform.rotation = Quaternion.Euler (new Vector3 (0f, 0f, newAngle));
+			transform.Rotate(Vector3.forward * deltaTheta);
+		}
+
+
+		// Calculate new velocity
 		velocity += acceleration * Time.deltaTime;
 		velocity = Vector2.ClampMagnitude (velocity, maxSpeed * Time.deltaTime);
 		rb.MovePosition (pos + velocity);
