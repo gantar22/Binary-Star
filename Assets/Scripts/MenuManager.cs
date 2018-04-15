b603bbfe9fc8d4294ae9f91473ca53a2e03a43be
Dmcdominic <dmcdominic@gmail.com>
2018-04-15 13:45:17 -0400

    Space Background Zoomed In. MenuManager is Back.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void StartGame() {
+		// TEMPORARY:
+		SceneManager.LoadScene ("EnemyTesting"); 
+	}
+
+	public void LoadMainMenu() {
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void Quit() {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit;
+		#endif
+	}
+}
