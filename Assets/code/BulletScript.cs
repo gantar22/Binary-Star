b88f55efba616d5a7016a448bf9b35dee4f67f2a
Dmcdominic <dmcdominic@gmail.com>
2018-04-19 13:47:04 -0400

    Bullets Can Only Hit One Thing (and Cancel all Hits if It Hits an Invulnerable). Explosions Improved

--- Assets/code/BulletScript.cs
+++ Assets/code/BulletScript.cs
@@ -11,42 +11,56 @@ public class BulletScript : MonoBehaviour {
 
 	private ObjT objT;
 
+	// Variables to make sure bullet only hits one thing
+	private GameObject objToHit;
+	private bool hitInvulnerable;
+
 
 	void Awake () {
 		objT = GetComponent<ObjT> ();
+		hitInvulnerable = false;
 	}
 
+	// Called once per frame, after all physics & collision checks
+	void Update () {
+		// If the bullet has collided with anything, none of which is invulnerable, then destroy one
+		if (objToHit != null) {
+			hitObject (objToHit);
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D col){
-
-		bull2 b = col.gameObject.GetComponent<bull2>();
-		if(b){
-			b.hit(transform.position);
+		if (hitInvulnerable) {
+			return;
 		}
 
 		Invulnerable I = col.gameObject.GetComponent<Invulnerable> ();
 		if (I != null && I.enabled) {
+			hitInvulnerable = true;
 			I.gotHit ();
 			die ();
+		} else {
+			objToHit = col.gameObject;
+		}
+	}
+
+	private void hitObject (GameObject obj) {
+		bull2 b = obj.GetComponent<bull2>();
+		if(b){
+			b.hit(transform.position);
 		}
 
-		/* BulletScript bs = col.gameObject.GetComponent<BulletScript> ();
-		if (bs != null) {
-			bs.die ();
-			die ();
-		} */
-
-		EnemyHP s = col.gameObject.GetComponent<EnemyHP>(); 
+		EnemyHP s = obj.GetComponent<EnemyHP>(); 
 		if (s != null){
 			s.gotHit(_damage);
 			die();
 		}
 
-		if(col.gameObject.layer == 9){
+		if(obj.layer == 9){
 			die();
 		}
 
-		PlayerHP PHP = col.gameObject.GetComponent<PlayerHP> ();
+		PlayerHP PHP = obj.GetComponent<PlayerHP> ();
 		if (PHP != null && (objT.typ != ObjT.obj.player_bullet)) {
 			PHP.gotHit (_damage);
 			die ();
