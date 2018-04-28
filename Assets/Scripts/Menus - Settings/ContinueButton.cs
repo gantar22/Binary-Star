799d5507ed7134c558af8a94b3bf95ace7a174d1
Dmcdominic <dmcdominic@gmail.com>
2018-04-28 17:03:06 -0400

    Continue Button and Start New Game (to restart) Buttons Implemented.  Freeplay wave tiers implemented

---
+++ Assets/Scripts/Menus - Settings/ContinueButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ContinueButton : MonoBehaviour {
+
+	private Button butt;
+	private Text text;
+
+	// Initialization
+	void Awake () {
+		butt = GetComponent<Button> ();
+		text = butt.GetComponentInChildren<Text> ();
+	}
+
+	// Check if there is any progress to continue from
+	void OnEnable () {
+		if (SpawnManager.Instance.sequenceIndex > 0) {
+			if (!butt.interactable) {
+				butt.interactable = true;
+				text.color = text.color + Color.black;
+			}
+		} else if (butt.interactable) {
+			butt.interactable = false;
+			text.color = text.color - (Color.black * 0.65f);
+		}
+	}
+}
--- Assets/Scripts/nextlevel.cs
+++ Assets/Scripts/nextlevel.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class nextlevel : MonoBehaviour {
 
-	// Use this for initialization
+	// Initialization
 	void Start () {
 		next();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 
 	void next(){
-		if(SceneManager.sceneCountInBuildSettings <= SceneManager.GetActiveScene().buildIndex + 1) {
+		SceneManager.LoadScene (GM.sceneAfterStart);
+
+		/* if(SceneManager.sceneCountInBuildSettings <= SceneManager.GetActiveScene().buildIndex + 1) {
 			//set ui end game active here
 			return;
 		}
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); */
 	}
 }
