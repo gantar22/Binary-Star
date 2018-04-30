2b85ed3c38967b78949f97170304221200563665
Dmcdominic <dmcdominic@gmail.com>
2018-04-30 18:18:37 -0400

    Main Menu Beautified

--- Assets/Scripts/Menus - Settings/ContinueButton.cs
+++ Assets/Scripts/Menus - Settings/ContinueButton.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class ContinueButton : MonoBehaviour {
 
 	private Button butt;
+	private Image image;
 	private Text text;
 
 	// Initialization
 	void Awake () {
 		butt = GetComponent<Button> ();
+		image = butt.GetComponent<Image> ();
 		text = butt.GetComponentInChildren<Text> ();
 	}
 
@@ -19,12 +21,22 @@ public class ContinueButton : MonoBehaviour {
 	void OnEnable () {
 		if (SpawnManager.Instance.sequenceIndex > 0) {
 			if (!butt.interactable) {
-				butt.interactable = true;
-				text.color = text.color + Color.black;
+				setInteractable (true);
 			}
 		} else if (butt.interactable) {
+			setInteractable (false);
+		}
+	}
+
+	private void setInteractable (bool setToTrue) {
+		if (setToTrue) {
+			butt.interactable = true;
+			text.color = text.color + Color.black;
+			image.color = image.color + Color.black;
+		} else {
 			butt.interactable = false;
 			text.color = text.color - (Color.black * 0.65f);
+			image.color = image.color - (Color.black * 0.65f);
 		}
 	}
 }
