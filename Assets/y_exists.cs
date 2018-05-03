9223ff40ade39a2ed9d9cb633ed325b7c73f092f
Nathan Carter Williams <nathancarterwilliams@gmail.com>
2018-05-03 04:04:32 -0400

    ui

---
+++ Assets/y_exists.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class y_exists : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < transform.childCount;i++){
+			transform.GetChild(i).gameObject.SetActive(YButtonManager.abilityIsUnlocked());
+		}
+	}
+}
